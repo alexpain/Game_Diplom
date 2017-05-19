116d6a2ab60e9995ad39a2f543258ac44d6fee28
alexpain <alex.pain.6@gmail.com>
2017-05-19 13:11:56 +0300

    alexpain
    create

---
+++ Assets/SwichScene.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using BitBenderGames;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class SwichScene : MonoBehaviour
+{
+    
+    private TouchInputController touchInputController;
+    public GameObject apartment;
+    public void Awake()
+    {
+        //apartment = FindObjectOfType<Camera>();
+        Application.targetFrameRate = 60;
+        touchInputController = apartment.GetComponent<TouchInputController>();
+        
+
+        touchInputController.OnFingerDown += OnFingerDown;
+
+    
+    }
+    private void OnFingerDown(Vector3 screenPosition)
+    {
+        Application.LoadLevel(1);
+
+    }
+
+}
