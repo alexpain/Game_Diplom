e75731178874806320713f02cc601687a77fe77b
alexpain <alex.pain.6@gmail.com>
2017-05-22 18:09:13 +0300

    Добавил рандом
    Теперь переход доступен только у одного дома из нескольких

---
+++ Assets/GenerateEvent.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEngine;
+using Random = System.Random;
+
+public class GenerateEvent : MonoBehaviour
+{
+    
+
+    private static int a;
+
+	public static int temp;
+
+    void Start ()
+    {
+        temp = 0;
+
+    }
+	
+	void Update ()
+	{
+        Random rand = new Random();
+	    
+	    if (temp == 0)
+	    {
+            temp = rand.Next(1000, 5000);
+            Debug.Log(temp);
+            
+        }
+        
+        a++;
+       
+	    if (a > temp)
+	    {
+            Debug.Log(a);
+	        a = 0;
+	        temp = 0;
+            SwichScene.evnt = 0;
+            SwichScene.flag = true;
+        }
+	    
+	}
+}
--- Assets/SwichScene.cs
+++ Assets/SwichScene.cs
@@ -1,19 +1,56 @@
-﻿
-using UnityEngine;
+﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using NUnit.Framework;
 using UnityEngine.UI;
+using Random = System.Random;
 
-
-    public class SwichScene : MonoBehaviour
+public class SwichScene : MonoBehaviour
+{
+    public GameObject item;
+    public static List<GameObject> list;
+    public static bool flag;
+    public static int evnt;
+    void Start()
     {
+        if (list == null)
+        {
+            list = new List<GameObject>();
+        }
+        
+        list.Add(item);
+        evnt = 0;
+        //Debug.Log(list.Count);
+    }
+
+    void Update()
+    {
+        Random rand = new Random();
+
+        if (evnt == 0)
+        {
+            evnt = rand.Next(0, list.Count);
+            Debug.Log(evnt);
+        }
+        if (flag && list[evnt] == item)
+        {
+            //item = GameObject.CreatePrimitive(PrimitiveType.Plane);
+            //item.renderer.material.mainTexture = Resources.Load(@"New Folder\Materials\road.mat");
+        }
+        
+    }
+
     void OnMouseDown()
     {
-        RaycastHit s = new RaycastHit();
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-        Vector2 oldPosition = new Vector2(transform.localPosition.x, transform.localPosition.z);
-        Debug.Log("RayCast On");
-        Application.LoadLevel(1);
+        Debug.Log(flag);
+        if (list[evnt] == item)
+        {
+            Debug.Log("asdasdasdasdasdasddddddddddddddddddddddddddd");
+        }
+        if (flag && list[evnt] == item)
+        {
+            flag = false;
+            Application.LoadLevel(1);
+        }
     }
 }
