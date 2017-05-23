ff8805c43d80a6847f9505b4234fb26fc569f892
Alexander Loginov <Alexandr010496@gmail.com>
2017-05-24 02:45:04 +0300

    Смена времени суток
    Добавлено время суток в сценах livehouse и simple city. Также в livehouse добавлена земля, чтобы всё не выглядело так ущербно. Сука, будто письма пишу. С уважением ваш Александр

---
+++ Assets/dayNightHouseScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class dayNightHouseScript : MonoBehaviour {
+    private double Rotate = 91;
+    // Use this for initialization
+    void Start () {
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        Quaternion spinZ = Quaternion.AngleAxis(90*(float)Math.Sin(Rotate), new Vector3(1, 0, 0));
+        Quaternion spinY = Quaternion.AngleAxis(270 + 60*(float)Math.Cos(Rotate), new Vector3(0, 1, 0));
+        transform.rotation = spinZ * spinY;
+        Rotate-=0.016666666666666;
+    }
+}
---
+++ Assets/dayNightScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dayNightScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        Quaternion spin = Quaternion.AngleAxis(1, new Vector3(0, 1, 0));
+        transform.rotation *= spin;
+    }
+}
