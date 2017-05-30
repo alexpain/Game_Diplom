6d05df2b4255c416a3c9ddd72424964327b15fa9
alexpain <alex.pain.6@gmail.com>
2017-05-30 11:05:18 +0300

    Добавил действия при тушении скавородки

---
+++ Assets/MyUI.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyUI : MonoBehaviour
+{
+    public GameObject Layout;
+    public static List<GameObject> ListUI;
+    void Start () {
+        Layout.gameObject.SetActive(false);
+        if (ListUI == null)
+        {
+            ListUI = new List<GameObject>();
+        }
+
+        ListUI.Add(Layout);
+    }
+	
+    public static void SetActive(bool status)
+    {
+        ListUI[5].gameObject.SetActive(status);
+        ListUI[4].gameObject.SetActive(status);
+        ListUI[3].gameObject.SetActive(status);
+        ListUI[2].gameObject.SetActive(status);
+    }
+
+    public static void GetPanel()
+    {
+        ListUI[1].gameObject.SetActive(true);
+    }
+    public static void GetPanelFail()
+    {
+        ListUI[0].gameObject.SetActive(true);
+    }
+    public static void HidePanelFail()
+    {
+        ListUI[5].gameObject.SetActive(false);
+        ListUI[4].gameObject.SetActive(false);
+        ListUI[3].gameObject.SetActive(false);
+        ListUI[2].gameObject.SetActive(false);
+        ListUI[1].gameObject.SetActive(false);
+        ListUI[0].gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Аction.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Аction : MonoBehaviour {
+
+    public void OnMouseDown()
+    {
+        Debug.Log("asdasd");
+        MyUI.SetActive(true);
+
+    }
+
+    public void Back()
+    {
+        Application.LoadLevel(0);
+    }
+
+    public void SetАction()
+    {
+        MyUI.SetActive(false);
+        MyUI.GetPanel();
+    }
+    public void GetPanelFail()
+    {
+        MyUI.SetActive(false);
+        MyUI.GetPanelFail();
+    }
+
+    public void HidePanelFail()
+    {
+        MyUI.HidePanelFail();
+    }
+
+
+}
